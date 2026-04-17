0457d1c82256bd5177158a695f418f9544169ae1
author <2384322990@qq.com>
2026-04-17 14:05:00 +0800

    feat: 更新数据导出，添加选项池导出
    - 导出 OPTION_POOLS 供其他模块使用
    - 保持其他导出不变

---
+++ js/data/index.js
@@ -0,0 +1,24 @@
+/**
+ * 数据统一导出入口
+ */
+
+// 运势数据
+export { fortunes } from './fortunes.js';
+
+// 事件数据
+export { events } from './events-data.js';
+
+// 选项池数据
+export { OPTION_POOLS } from './option-pools.js';
+
+// 成就数据
+export { achievements } from './achievements.js';
+
+// 结局片段数据
+export { 
+    sanityFragments, 
+    stressFragments, 
+    moneyFragments, 
+    endingFragments, 
+    defaultEnding 
+} from './endings.js';
